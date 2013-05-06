43080264f4b815a657028e04b6551bb8aeea1e82
WangHuan <wanghuan19861103@gmail.com>
2013-05-06 23:48:47 +0800

    fix bug

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -4,57 +4,6 @@
 **/
 
 $.ns('UI.Canlendar');
-/*
-var tmpl = {
-	
-	frame : '<div class="calendar-skin-wrapper">' +
-				'<%=head%>' + '<%=body%>' +
-			'</div>',
-	
-	head :  '<div class="calendar-head clearfix">' +
-				'<span class="left-btn">&lt;</span>' +
-				'<div class="year-month">' +
-					'<span class="year"><%=year%></span>' +
-					'<span>年</span>' +
-					'<span class="month"><%=month%></span>' +
-					'<span>月</span>' +
-				'</div>' +
-				'<span class="right-btn">&gt;</span>' +
-			'</div>',
-			
-	body :  '<div class="calendar-body">' +
-				 '<table class="calendar-body">' +
-					 '<tbody>' +
-						  '<tr class="weekmark">' +
-							 '<td>日</td>' +
-							 '<td>一</td>' +
-							 '<td>二</td>' +
-							 '<td>三</td>' +
-							 '<td>四</td>' +
-							 '<td>五</td>' +
-							 '<td>六</td>' +               
-						 '</tr>' +
-						 '<% var count = 0; %> ' +
-						  '<% for(var i = 0;i < days.length ;i++){ %>' +  //days : [{year:2013,month:5,day:1,notCurrentMonDay : true}]
-							  '<%	if(count % 7 == 0){ %>' +
-									 '<tr>' +
-							  '<%	} %>' +
-							  '<%	if(days[i].notCurrentMonDay){ %>' +
-									 '<td class="not-currentmonth-day"><%=days[i].day%></td>' +
-							  '<%	} %>' +
-							  '<%	else{ %>' +
-									 '<td><%=days[i].day%></td>' +
-							  '<%  } %>' +
-							  '<%	if(count % 7 == 0){ %>' +
-									 '</tr>' +
-							  '<%	} %>' +
-							  '<%	count++ %>' +
-						  '<% } %>' +
-					'</tbody>'+
-			  '</table>'+
-			'</div>'
-};
-*/
 
 var tmpl = {
 	
@@ -73,8 +22,7 @@ var tmpl = {
 				'<span class="right-btn">&gt;</span>' +
 			'</div>',
 			
-	body :  '<div class="calendar-body">' +
-				 '<table class="calendar-body">' +
+	body :  '<table class="calendar-body">' +
 					 '<tbody>' +
 						  '<tr class="weekmark">' +
 							 '<td>日</td>' +
@@ -87,11 +35,21 @@ var tmpl = {
 						 '</tr>' +
 						 '<% var count = 0; %> ' +
 						  '<% for(var i = 0;i < days.length ;i++){ %>' +  //days : [{year:2013,month:5,day:1,notCurrentMonDay : true}]
-								'<%=count%>' +
+							  '<%	if(count % 7 == 0){ %>' +
+									 '<tr>' +
+							  '<%	} %>' +
+							  '<%	if(days[i].notCurrentMonDay){ %>' +
+									 '<td class="not-currentmonth-day"><%=days[i].day%></td>' +
+							  '<%	}else{ %>' +
+									 '<td><%=days[i].day%></td>' +
+							  '<%  } %>' +
+							  '<%	count++; %>' +
+							  '<%	if(count % 7 == 0){ %>' +
+									 '</tr>' +
+							  '<%	} %>' +
 						  '<% } %>' +
 					'</tbody>'+
-			  '</table>'+
-			'</div>'
+			  '</table>'
 };
 
 (function($,window){
